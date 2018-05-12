daa9b746732dc487bb6d43af84b014692f265758
tt315 <noodles-one@.github.com>
2018-05-12 16:15:39 +0800

    第十六天

---
+++ 第十六天/js/main.js
@@ -0,0 +1,12 @@
+document.getElementById("first").onclick = function() {
+    var css = document.getElementById("link")
+    css.setAttribute("href", "css/style_1.css")
+}
+document.getElementById("second").onclick = function() {
+    var css = document.getElementById("link")
+    css.setAttribute("href", "css/style_2.css")
+}
+document.getElementById("third").onclick = function() {
+    var css = document.getElementById("link")
+    css.setAttribute("href", "css/style_3.css")
+}
